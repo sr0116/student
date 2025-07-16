bad7aa7e41b95c4fd6d7f1940e9589684bdf898b
sr0116 <kysy0116@gmail.com>
2025-07-16 19:32:34 +0900

    student

---
+++ src/main/java/com/imchobo/jsr_stringboot/JsrStringBootApplication.java
@@ -0,0 +1,13 @@
+package com.imchobo.jsr_stringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsrStringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JsrStringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/imchobo/jsr_stringboot/JsrStringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imchobo.jsr_stringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsrStringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
